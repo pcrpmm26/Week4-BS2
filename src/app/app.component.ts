90bb183f6c342adf12627c89b6ab2fe320fb620a
pcrpmm26 <maysexrn@gmail.com>
2021-11-23 16:53:26 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BS2';
+  n1: number =5;
+  n2: number =2;
+  picName:string = "http://xn--12c4bmrju6hc9d9j.com/wp-content/uploads/2015/04/sunflower-vector-design.jpg";
+
+  // Func
+  hi(name:string){
+    alert("Hi Hi!!"+ name);
+  }
 }
